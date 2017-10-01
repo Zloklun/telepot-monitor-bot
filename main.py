a8c95bec179feb0d192c832a58e5082ea8cbb699
Azat Rafikov <Azat@Rafikov.me>
2017-10-01 17:03:22 +0300

    Simple chat_id filtering

--- main.py
+++ main.py
@@ -9,10 +9,21 @@ from telepot.loop import MessageLoop
 token = open(join(dirname(__file__), 'TOKEN')).read().strip()
 bot = telepot.Bot(token)
 
+whitelist = None
+try:
+	with open(join(dirname(__file__), 'whitelist_ids')) as f:
+		whitelist = map(int, f.read().split())
+except IOError:
+	pass
+
 def handle_message(msg):
 	content_type, chat_type, chat_id = telepot.glance(msg)
 	print(content_type, chat_type, chat_id)
 	print(msg)
+
+	if whitelist is not None and chat_id not in whitelist:
+		print('Unrecognized chat_id. Drop')
+		bot.sendMessage(chat_id, 'Not in whitelist')
 
 	if content_type == 'text':
 		bot.sendMessage(chat_id, msg['text'])
