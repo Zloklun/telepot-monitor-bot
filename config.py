3bba78f94468e8229b20ce9351c7f9aba81d168c
Zloklun <Zloklun@hotmail.com>
2017-10-03 03:17:49 +0500

    1. Small style changes

--- config.py
+++ config.py
@@ -3,9 +3,7 @@
 from os.path import join, dirname
 
 DEBUG = True
-
 TOKEN_FILE = join(dirname(__file__), 'TOKEN')
-
 WHITELIST_FILE = join(dirname(__file__), 'whitelist_ids')
 
 
--- inotifier.py
+++ inotifier.py
@@ -3,8 +3,6 @@
 import asyncio as aio
 import pyinotify as pyi
 
-from config import WHITELIST_FILE
-
 
 class InotifyEventHandler(pyi.ProcessEvent):
     def my_init(self, loop=None):
@@ -15,9 +13,6 @@ class InotifyEventHandler(pyi.ProcessEvent):
 
     def process_IN_ATTRIB(self, event):
         print("ATTRIB event:", event.pathname)
-
-    def process_IN_CLOSE_NOWRITE(self, event):
-        print("CLOSE_NOWRITE event:", event.pathname)
 
     def process_IN_CLOSE_WRITE(self, event):
         print("CLOSE_WRITE event:", event.pathname)
@@ -31,14 +26,11 @@ class InotifyEventHandler(pyi.ProcessEvent):
     def process_IN_MODIFY(self, event):
         print("MODIFY event:", event.pathname)
 
-    def process_IN_OPEN(self, event):
-        print("OPEN event:", event.pathname)
-
 
 async def inotify_start(loop, files, event_mask=None):
-    files = files or [WHITELIST_FILE]
     event_mask = event_mask or \
-                      pyi.IN_MODIFY | pyi.IN_ATTRIB | pyi.IN_CLOSE_WRITE
+                      pyi.IN_MODIFY | pyi.IN_ATTRIB | pyi.IN_CLOSE_WRITE | \
+                      pyi.IN_DELETE | pyi.IN_CREATE
     wm = pyi.WatchManager()
     wm.add_watch(files, event_mask)
     handler = InotifyEventHandler(loop=loop)
