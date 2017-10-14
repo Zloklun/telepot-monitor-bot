50125cb5698c28c654eddd850adfe37ba3eb9ba2
Zloklun <Zloklun@hotmail.com>
2017-10-14 21:30:33 +0500

    Added misc.per_admin function

--- misc.py
+++ misc.py
@@ -1,7 +1,10 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 *-*
+
 import asyncio as aio
 
 from os.path import join, dirname
-
+from telepot.aio.delegate import exception
 
 def sync_exec(coro):
     aio.ensure_future(coro)
@@ -36,6 +39,7 @@ if DEBUG:
             print(*args, file=stderr)
 
 
+# Filling whitelist
 WHITELIST = None
 try:
     with open(WHITELIST_FILE) as f:
@@ -45,6 +49,7 @@ except IOError:
     log('Whitelist not found. Filtering is off')
 
 
+# Filling admin list
 ADMINS_LIST = None
 try:
     with open(ADMINS_FILE) as f:
@@ -53,3 +58,33 @@ try:
 except IOError:
     log('Admins list have not found. No admins supported')
 
+
+# Some seeder functions
+
+def _wrap_none(fn):
+    """
+    Stolen from telepot.aio.delegate.py
+    :param fn: function to wrap
+    :return: wrapped function
+    """
+    def w(*args, **kwargs):
+        try:
+            return fn(*args, **kwargs)
+        except (KeyError, exception.BadFlavor):
+            return None
+    return w
+
+
+def per_admin():
+    """
+    :return:
+        a seeder function that returns the from id only if the from id is in
+        the ``ADMINS_LIST``. If ADMINS_LIST is None, returns None.
+    """
+    if ADMINS_LIST is not None:
+        return None
+    else:
+        return _wrap_none(lambda msg:
+                          msg['from']['id']
+                          if msg['from']['id'] in ADMINS_LIST
+                          else None)
