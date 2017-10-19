86b2770539057abeea45a016457ad68b47a2d5fa
Zloklun <Zloklun@hotmail.com>
2017-10-19 16:28:16 +0500

    Cleaned AdminBot code

--- adminbot.py
+++ adminbot.py
@@ -15,10 +15,7 @@ from chatbot import ADMIN_SENDERS
 
 class AdminBot(Monitor):
     def __init__(self, seed_tuple, admins, *args, **kwargs):
-        super(AdminBot, self).__init__(
-                seed_tuple,
-                capture=[[lambda msg: not is_event(msg)]]
-        )
+        super(AdminBot, self).__init__(seed_tuple, capture=[])
         loop = aio.get_event_loop()
         loop.create_task(
                 loadavg.LoadavgNotifier(
@@ -35,35 +32,18 @@ class AdminBot(Monitor):
         )
         misc.log('__init__', category='AdminBot')
 
-    def on__idle(self):
-        pass
-
     async def on_chat_message(self, msg):
         """Handles chat message"""
         pass
 
     async def send_to_admins(self, message: str):
-        """Sends message to all chats in whitelist"""
+        """Sends message to all admins"""
         if not ADMIN_SENDERS:
             misc.log('ADMIN_SENDERS is empty', category='ChatBot')
             return False
         for _, sender in ADMIN_SENDERS:
             await sender.sendMessage(message, parse_mode='Markdown')
             return True
-
-    async def route_command(self, chat_id, message: str):
-        """Routes command to appropriate function"""
-        cmd, *args = message.split()
-        assert isinstance(cmd, str)
-        cmd = cmd.lower()
-        if cmd in self.routes.keys():
-            await self.send_if_admin(
-                    chat_id,
-                    self.routes[cmd](cmd, *args),
-            )
-        else:
-            await self.send_if_admin(chat_id, 'Wrong command')
-            await self.route_command('/help')
 
     async def loadavg_notify(self, loadavg):
         await self.send_to_admins('*High loadavg value*:\n' + str(loadavg))
