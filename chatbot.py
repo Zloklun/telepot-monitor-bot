9ce7d70ab440bd3dea5cd60b372c0e04cd73bd18
Zloklun <Zloklun@hotmail.com>
2017-10-28 19:22:51 +0500

    Extract ChatBot.uptime and ChatBot.random_number to separate functions

--- chatbot.py
+++ chatbot.py
@@ -11,6 +11,70 @@ import misc
 ADMIN_SENDERS = []
 
 
+def random_number(cmd, *args):
+    """Returns random number"""
+    usage = 'Usage: *{}* \[start] \[end]'.format(cmd)
+    import random
+    if len(args) == 0:
+        return str(random.random())
+    elif len(args) == 1:
+        arg = args[0]
+        if arg == 'help':
+            return usage
+        try:
+            arg = int(arg)
+            return random.randint(1, arg)
+        except ValueError:
+            return 'Wrong command.\n' + usage
+    elif len(args) == 2:
+        try:
+            a, b = args
+            a = int(float(a))
+            b = int(float(b))
+            if a > b:
+                return usage
+            else:
+                return random.randint(a, b)
+        except ValueError:
+            return usage
+    else:
+        return usage
+
+
+def uptime(cmd, *args):
+    """Uptime info"""
+    usage = 'Usage: {} \[units]\n' \
+            'Supported units are ' \
+            'sec, min, hour, days and weeks ' \
+            '(only first letter considered)'.format(cmd)
+    if not args:
+        args = ['d']
+    if args and args[0][0].lower() in 'smhdw' and args[0] != 'help':
+        seconds = float(open('/proc/uptime').read().split()[0])
+        unit = args[0][0].lower()
+        full_units = {
+            's': 'seconds',
+            'm': 'minutes',
+            'h': 'hours',
+            'd': 'days',
+            'w': 'weeks',
+        }
+        if unit == 's':
+            value = seconds
+        elif unit == 'm':
+            value = seconds / 60
+        elif unit == 'h':
+            value = seconds / 3600
+        elif unit == 'd':
+            value = seconds / 3600 / 24
+        elif unit == 'w':
+            value = seconds / 3600 / 24 / 7
+        else:
+            return usage
+        return '*Uptime*: {:.3f} {}'.format(value, full_units[unit])
+    return usage
+
+
 class ChatBot(UserHandler):
     """Bot that handles non-admin commands"""
     def __init__(self, seed_tuple, exclude=None, *args, **kwargs):
@@ -20,10 +84,10 @@ class ChatBot(UserHandler):
         self.routes = {
             '/start': self.start,
             '/help': self.start,
-            '/random': self.random_number,
+            '/random': random_number,
         }
         self.admin_routes = {
-            '/uptime': self.uptime,
+            '/uptime': uptime,
         }
         if self.user_is_admin():
             ADMIN_SENDERS.append((self.user_id, self.sender))
@@ -96,71 +160,9 @@ class ChatBot(UserHandler):
 
         return 'Available user commands are:\n' + user_cmds + admin_cmds
 
-    def random_number(self, cmd, *args):
-        """Returns random number"""
-        usage = 'Usage: *{}* \[start] \[end]'.format(cmd)
-        import random
-        if len(args) == 0:
-            return str(random.random())
-        elif len(args) == 1:
-            arg = args[0]
-            if arg == 'help':
-                return usage
-            try:
-                arg = int(arg)
-                return random.randint(1, arg)
-            except ValueError:
-                return 'Wrong command.\n' + usage
-        elif len(args) == 2:
-            try:
-                a, b = args
-                a = int(float(a))
-                b = int(float(b))
-                if a > b:
-                    return usage
-                else:
-                    return random.randint(a, b)
-            except ValueError:
-                return usage
-        else:
-            return usage
-
     async def send_if_admin(self, message: str):
         """Sends message to all chats in whitelist"""
         if not self.user_is_admin():
             return False
         await self.sender.sendMessage(message, parse_mode='Markdown')
         return True
-
-    def uptime(self, cmd, *args):
-        """Uptime info"""
-        usage = 'Usage: {} \[units]\n' \
-                'Supported units are ' \
-                'sec, min, hour, days and weeks ' \
-                '(only first letter considered)'.format(cmd)
-        if not args:
-            args = ['d']
-        if args and args[0][0].lower() in 'smhdw' and args[0] != 'help':
-            seconds = float(open('/proc/uptime').read().split()[0])
-            unit = args[0][0].lower()
-            full_units = {
-                's': 'seconds',
-                'm': 'minutes',
-                'h': 'hours',
-                'd': 'days',
-                'w': 'weeks',
-            }
-            if unit == 's':
-                value = seconds
-            elif unit == 'm':
-                value = seconds / 60
-            elif unit == 'h':
-                value = seconds / 3600
-            elif unit == 'd':
-                value = seconds / 3600 / 24
-            elif unit == 'w':
-                value = seconds / 3600 / 24 / 7
-            else:
-                return usage
-            return '*Uptime*: {:.3f} {}'.format(value, full_units[unit])
-        return usage
